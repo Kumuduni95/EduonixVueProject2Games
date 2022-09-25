7ab7fc1c42960e79620d73caff4ead4fda26b4f5
Kumuduni Jayakody <kumudujayakody25@gmail.com>
2022-09-25 19:34:20 +0530

    about page

--- src/views/AboutProject.vue
+++ src/views/AboutProject.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Project 2</h1>
+    <h2>Create a collection of little games</h2>
+    <h3>Created For Eduonix JavaScript Frameworks Mini E-Degree  <br><br></h3>
+    <h3 style="text-align:left; margin-left:20px">
+        Instructions:<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;Create a collection of little games (Done)<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;Each game should have its own page. (Done)<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;Do this one if you’re creative! (Done)<br>
+        &nbsp;&nbsp;&nbsp;&nbsp;As a bonus deploy your project in any of the 3 ways you learned in section 8 (Done)
+        </h3>
   </div>
 </template>
